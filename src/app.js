4eca87357b930d8ee4d3fc727acce374755d9f77
Shashank Mishra <shashankmishra3082000@gmail.com>
2026-03-15 17:17:43 +0530

    added live frontend url

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const cors=require('cors');
 
 const app=express();
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    origin: ['http://localhost:5173', 'http://localhost:5174',"https://spotifyclonebasic.vercel.app"],
     credentials: true
 }));
 app.use(express.json())
